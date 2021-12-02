bf6210507a648efee8c919dcb8318afa36d6cc80
Jordan Dennis <jdenn105@gmail.com>
2021-12-02 12:47:58 +1000

    Supressing output

--- Guttler 2014 TickTack.jl
+++ Guttler 2014 TickTack.jl
@@ -4,5 +4,5 @@ ticktack = pyimport("ticktack");
 
 cbm = ticktack.load_presaved_model("Guttler14", production_rate_units="atoms/cm^2/s");
 
-cbm.compile()
-cbm.equilibrate(production_rate=1.88)
+cbm.compile();
+cbm.equilibrate(production_rate=1.88);
