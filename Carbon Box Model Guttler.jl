03ca066b3a70a3dfda1fcae85d5fc63728e1dc59
Jordan Dennis <jdenn105@gmail.com>
2021-12-04 19:39:34 +1000

    Finalising the profile_solvers function

--- Carbon Box Model Guttler.jl
+++ Carbon Box Model Guttler.jl
@@ -83,15 +83,15 @@ function profile_solvers(solvers::Vector)::Matrix{Union{Float64, String}}
         for i in 1:10
             local timer = time();                               # Starting a timer
             solution = run_solver(solver(), ∇, burn_in[end]);   # Running the solver
-            time_vector[i] = -timer + time();                   # ending the timer 
+            time_sample[i] = -timer + time();                   # ending the timer 
         end
-        time_meane[index] = mean(time_vector);  # Storing run time 
-        time_var[index] = var(time_vector);     # Storing time error
+        time_mean[index] = mean(time_sample);  # Storing run time 
+        time_var[index] = var(time_sample);     # Storing time error
     end 
 
-    C14 = (C14' .- median(C14, dims=1)')';      # Calculating deviations from median 
+    C14 = C14' .- median(C14, dims=1)';         # Calculating deviations from median 
     results.accuracy = mean(C14, dims=2);       # Calculating the mean of the deviation from the median 
-    results.accuracy_error = var(C14, dims=2);  # Calculating the RMSE error << is better 
+    results.accuracy_var = var(C14, dims=2);    # Calculating the RMSE error << is better 
     results.time_mean = time_mean;              # Storing the mean run time
     results.time_var = time_var;                # Storing the varience of the time sample 
     return results
@@ -99,13 +99,13 @@ end
 
 # function main()
     solvers = [Rosenbrock23, ROS34PW1a, QNDF1, ABDF2, ExplicitRK,
-        DP5, TanYam7, Vern6, SSPRK43, VCAB5];    # A list of solvers
-    solver_info = profile_solvers(solvers);      # Calling the program
+        DP5, TanYam7, Vern6, SSPRK43, VCAB5];   # A list of solvers
+    r = profile_solvers(solvers);         # Calling the program
 
     datavisual = Gadfly.plot(
-        y=accuracy, x=time, label=labels,   # Some data 
-        Guide.ylabel("Accuracy (RMSE)"),    # X Label
-        Guide.xlabel("Time (s)"),           # Y Label
-        ymin=accuracy - acc_err, ymax=accuracy + acc_err, # Accuracy error bars
-        Geom.yerrorbar, Geom.point, Geom.label)   
+        y=r.accuracy, x=r.time_mean, label=r.solvers,  
+        Guide.ylabel("Accuracy"), Guide.xlabel("Time"),          
+        ymin=r.accuracy - r.accuracy_var, ymax=r.accuracy + r.accuracy_var,
+        xmin=r.time_mean - r.time_var, xmax=r.time_mean + r.time_var, 
+        Geom.yerrorbar, Geom.xerrorbar, Geom.point, Geom.label)   
 # end
