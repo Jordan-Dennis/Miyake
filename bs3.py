0f85f926f565c3725523de37f7c647d197802710
Jordan Dennis <jdenn105@gmail.com>
2021-12-09 11:58:52 +1000

    implementing a testing algorithm

--- bs3.py
+++ bs3.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # I am going to start implementing this as a function. This will need to be included within a class later on anyway. 
 
-def bs3(x0: np.array, t: np.array, f: function, reltol: np.float64):
+def bs3(x0: np.array, t: np.array, f, reltol: np.float64):
     """
     It is assumed that dy/dt = f(t, x_t). For now anyway 
     """
@@ -23,3 +23,25 @@ def bs3(x0: np.array, t: np.array, f: function, reltol: np.float64):
         h = 0.9 * h * abs(x[i + 1] - z) / reltol    # New timestep 
     
     return x
+
+# Testing architecture 
+import ticktack
+
+cbm = ticktack.load_presaved_model("Guttler14", production_rate_units="atoms/cm^2/s")
+cbm.compile()
+x0 = cbm.equilibrate(production_rate=1.88)
+
+def production(t: np.float64, p: np.array):
+    """
+    The production function of the system
+    """
+    return p[1] * (1 + p[2] * np.sin(2 * np.pi / p[3] * t + p[4])) + \
+        p[5] * np.exp(- (p[6] * (t - 775) ^ 16))
+
+def dydx(t: np.float64, x: np.array):
+    """
+    The derivative of the system
+    """
+    parameters = np.zeros(6)    # array for the parameters of the production function
+    projection = np.zeros(11)   # Array for the projection of the production function
+    cbm._matrix * x + np.production(t, parameters)
