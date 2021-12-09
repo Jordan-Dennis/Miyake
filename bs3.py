a4b058a43b982360bfcddf0347b8a12270088bd3
Jordan Dennis <jdenn105@gmail.com>
2021-12-09 10:46:04 +1000

    Birth of python implementation of the bs3 algorithm

---
+++ bs3.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# I am going to start implementing this as a function. This will need to be included within a class later on anyway. 
+
+def bs3(x0: np.array, t: np.array, f: function)
